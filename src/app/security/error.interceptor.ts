161c9454d774ffbcf14ca5a69f53950da9240e4d
Devayan Sarkar <devayansarkar@gmail.com>
2020-05-13 17:10:47 +0200

    updating error handlers

---
+++ src/app/security/error.interceptor.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import ILoggedInUser from '../models/auth/ILoggedInUser';
+import { logoutEvent } from '../store/auth/auth.actions';
+
+@Injectable()
+export class AppErrorInterceptor implements HttpInterceptor {
+
+    constructor(private _store: Store<{ authenticatedUser: ILoggedInUser }>) {
+    }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(request).pipe(catchError(err => {
+            if ([401, 403].indexOf(err.status) !== -1) {
+                this._store.dispatch(logoutEvent())
+                location.reload(true);
+            }
+            const error = err.error.message || err.statusText;
+            return throwError(error);
+        }))
+    }
+}
