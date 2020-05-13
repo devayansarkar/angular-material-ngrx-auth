ebab4541c9409306c7ab0224b44211fba262b924
Devayan Sarkar <devayansarkar@gmail.com>
2020-05-13 16:58:31 +0200

    Account placeholder page for logged in users

---
+++ src/app/modules/account/account.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AccountComponent } from './account.component';
+
+
+
+@NgModule({
+  declarations: [AccountComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AccountModule { }
