cfcc89f1da7218893c30dc01f1fdb395e3b62f25
Devayan Sarkar <devayansarkar@gmail.com>
2020-05-13 17:06:40 +0200

    adding security

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './modules/login/login.component';
-
+import { HomeComponent } from './modules/home/home.component';
+import { AccountComponent } from './modules/account/account.component';
+import { AuthGuardService } from '../app/security/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'account', component: AccountComponent, canActivate: [AuthGuardService] },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
---
+++ src/app/security/http.interceptor.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Store, select } from '@ngrx/store';
+import ILoggedInUser from '../models/auth/ILoggedInUser';
+import { HttpInterceptor, HttpRequest, HttpEvent, HttpHandler } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AppHttpClientHttpInterceptor implements HttpInterceptor {
+
+    authenticatedUser: ILoggedInUser = { isLoggedIn: false }
+
+    constructor(private _http: Http, private _store: Store<{ authenticatedUser: ILoggedInUser }>) {
+        this._store.pipe(select('authenticatedUser')).subscribe((item) => {
+            this.authenticatedUser = item
+        })
+    }
+    
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (this.authenticatedUser.isLoggedIn) {
+            request = request.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${this.authenticatedUser.token}`,
+                    uuid: this.authenticatedUser.uuid
+                }
+            });
+        }
+        return next.handle(request);
+    }
+}
